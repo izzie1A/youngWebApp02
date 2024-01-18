e8e4df717ca89a70d7dfebe2841e578ceef60aff
izzie1A <47637788+izzie1A@users.noreply.github.com>
2024-01-18 22:52:06 +0000

    carosel add dot display

--- src/app/components/carosel/carosel.component.ts
+++ src/app/components/carosel/carosel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-carosel',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./carosel.component.css']
 })
 export class CaroselComponent {
- li = [0,0,0]
+  @ViewChild('scrollableDiv') scrollableDiv: ElementRef | undefined;
+  @ViewChild('scrollableDiv2') scrollableDiv2: ElementRef | undefined;
+  li = [0, 0, 0, 0, 0, 0];
+  scrollposition:number = 0;
+  getScrollPosition() {
+    const scrollPosition = this.scrollableDiv?.nativeElement.scrollTop;
+    let answer = scrollPosition / this.scrollableDiv?.nativeElement.scrollHeight;
+    console.log('Scroll Position:', answer);
+    // console.log('Scroll Position:', scrollPosition);
+  }
+  getScrollPosition2() {
+    // console.log('Scroll Position:', answer);
+    const scrollPosition2 = this.scrollableDiv2?.nativeElement.scrollLeft;
+    const totalScrollableWidth = this.scrollableDiv2?.nativeElement.scrollWidth;
+    let ans = scrollPosition2 / totalScrollableWidth
+    this.scrollposition =Math.floor(ans*this.li.length);
+  }
 }
