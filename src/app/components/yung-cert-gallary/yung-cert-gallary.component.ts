583e5a0c3c2e1a55a9ebcbe8582fa4f134b7635e
izzie1A <47637788+izzie1A@users.noreply.github.com>
2024-01-20 02:43:27 +0000

    homepageStyle gallery update

--- src/app/components/yung-cert-gallary/yung-cert-gallary.component.ts
+++ src/app/components/yung-cert-gallary/yung-cert-gallary.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseControlService } from "src/app/services/firebase-control.service";
 
 @Component({
   selector: 'app-yung-cert-gallary',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./yung-cert-gallary.component.css']
 })
 export class YungCertGallaryComponent {
-  pageindex:number=0;  
-
+  pageindex: number = 0;
+  pageItemArray: any[] = [];
+  constructor(private fbs: FirebaseControlService) {
+    // const clientCollection = collection(this.firestore, '/yungFolder/ourClient/clientReferences');
+    // this.clientList$ = collectionData(clientCollection);
+    this.t();
+  }
+  async t() {
+    let result = await this.fbs.queryCondition('/yungFolder/ourClient/clientReference', 100, "name", "!=", 'null', 'name');
+    let pageItemlength = 9;
+    this.pageItemArray = result;
+    // for (let i = 0; i < result.length; i++) {
+    //   let subArray = []
+    //   for (let j = 0; j < pageItemlength; j++) {
+    //     i++;
+    //     console.log(i);
+    //     subArray.push(result[i]);
+    //   }
+    //   this.pageItemArray.push(subArray);
+    // }
+  }
+  switchPage(inputInt: number) {
+    if (this.pageindex + inputInt > this.pageItemArray.length-1) {
+      this.pageindex = 0;
+    } else if (this.pageindex + inputInt < 0) {
+      this.pageindex = this.pageItemArray.length - 1;
+    } else if (this.pageindex + inputInt < this.pageItemArray.length) {
+      this.pageindex = this.pageindex + inputInt;
+    }
+  }
 }
